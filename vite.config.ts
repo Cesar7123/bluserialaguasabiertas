c4aa6c1e1398a3fee7aa104dabcc49ed272271bb
César Hernández <diverzdenweb@gmail.com>
2026-02-05 22:56:27 -0700

    corrected vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
-    react(),
+    react({
+      jsxRuntime: 'classic' // Add this line
+    }),
     tailwindcss(),
   ],
   resolve: {
